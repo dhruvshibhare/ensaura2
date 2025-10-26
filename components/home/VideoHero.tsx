a79dd859986f6c75680c8af23dec7a8cdc1a75c8
dhruvshibhare <shivharedhruv70@gmail.com>
2025-10-26 12:25:11 +0530

    Update VideoHero.tsx

--- components/home/VideoHero.tsx
+++ components/home/VideoHero.tsx
@@ -29,7 +29,7 @@ export default function VideoHero() {
             transition={{ duration: 0.8, delay: 0.2 }}
           >
             Illuminate Your
-            <span className="block text-white uppercase tracking-wider">AURO DROP</span>
+            <span className="block text-white uppercase tracking-wider">AURA DROP</span>
           </motion.h1>
           
           <motion.div
